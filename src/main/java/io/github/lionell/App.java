1caad741b3b8ae9555cff02d2e85a9b684f79b6d
Ruslan Sakevych <xlionell@gmail.com>
2015-11-30 03:41:57 +0200

    Add nested Reader class to App.

--- src/main/java/io/github/lionell/App.java
+++ src/main/java/io/github/lionell/App.java
@@ -3,7 +3,14 @@ package io.github.lionell;
 import ua.yandex.shad.stream.AsIntStream;
 import ua.yandex.shad.stream.IntStream;
 
-import java.util.Scanner;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 /**
  * Created by lionell on 11/29/15.
@@ -12,22 +19,64 @@ import java.util.Scanner;
  */
 public class App {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello from App!");
-        System.out.print("Enter elements count > ");
+        Reader in = new Reader();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+        out.println("Hello from App!");
+        out.print("Enter elements count > ");
         int n = in.nextInt();
-        System.out.print("Enter elements > ");
+        out.print("Enter elements > ");
         int[] ints = new int[n];
         for (int i = 0; i < n; i++) {
             ints[i] = in.nextInt();
         }
-        in.close();
         IntStream intStream = AsIntStream.of(ints);
         int res = intStream
                 .filter(x -> x > 0)
                 .map(x -> x * x)
                 .flatMap(x -> AsIntStream.of(x - 1, x, x + 1))
                 .reduce(0, (sum, x) -> sum += x);
-        System.out.println("Result: " + res);
+        out.println("Result: " + res);
+        in.close();
+        out.close();
+    }
+
+    private static class Reader {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public Reader() {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public Reader(String s) {
+            try {
+                reader = new BufferedReader(new FileReader(s));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String nextToken() {
+            while (tokenizer == null || !tokenizer.hasMoreElements()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextToken());
+        }
+
+        public void close() {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
